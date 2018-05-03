4a345b31feffc52baba1baed2ea68aa6417f779a
bobo19870225 <rain19870225@163.com>
2018-05-03 10:15:06 +0800

    自定义视图勒

--- app/src/main/java/com/jinkan/www/fastandroid/model/Movie.java
+++ app/src/main/java/com/jinkan/www/fastandroid/model/Movie.java
@@ -9,12 +9,9 @@ import java.util.List;
 public class Movie {
     private String title;
     private List<Subjects> subjects;
-
-
     public String getTitle() {
         return title;
     }
-
     public List<Subjects> getSubjects() {
         return subjects;
     }
