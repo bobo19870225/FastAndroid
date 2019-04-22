9f49ce2e9fb1ef34058c69344aa73c4a21974682
bobo19870225 <rain19870225@163.com>
2019-04-23 01:56:02 +0800

    天坑

---
+++ app/src/main/java/com/jinkan/www/fastandroid/view/adapter/Item.java
@@ -0,0 +1,31 @@
+package com.jinkan.www.fastandroid.view.adapter;
+
+/**
+ * Created by Sampson on 2019-04-22.
+ * FastAndroid
+ */
+public class Item<T> {
+    private T data;
+    private int type;
+
+    public Item(T data, int type) {
+        this.data = data;
+        this.type = type;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
